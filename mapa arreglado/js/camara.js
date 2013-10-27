2b0d430418f0d9634ec5fa515ce3d0beecf1a27a
Julián R Figueroa <julianrfigueroa@gmail.com>
2013-10-27 08:51:43 -0500

    traido del otro repo

---
+++ mapa arreglado/js/camara.js
@@ -0,0 +1,77 @@
+// JavaScript Document
+
+var licenseBool = false
+var type = 'no_type'
+var comment = 'no_comment'
+var imageData = 'no_image'
+var urlForm = 'http://www.aktio.co/eltopo/register.php'
+
+  function takePhoto() {
+    navigator.camera.getPicture(onCameraSuccess, onCameraError, {
+      quality: 10,
+      destinationType: Camera.DestinationType.FILE_URI
+    })
+  }
+
+  function onCameraSuccess(imageURI) {
+    console.log(imageURI)
+    ic = document.getElementById('image')
+    ic.src = imageURI
+
+    document.getElementById('photoBtn').value = 'Retomar foto'
+    $('#photoBtn').button('refresh')
+  }
+
+  function checkImgData(){
+    var c = document.getElementById('canvas')
+    c.height = ic.naturalHeight
+    c.width = ic.naturalWidth
+    var ctx = c.getContext('2d')
+    ctx.drawImage(ic, 0, 0)
+    imageData = c.toDataURL()
+
+    console.log(imageData)
+  }
+
+  function onCameraError(e) {
+    console.log(e)
+    navigator.notification.alert('onCameraError: ' + e)
+  }
+
+  function sendInfo() {
+    if ($('#license').is(':checked')) {
+      licenseBool = true
+    } else {
+      licenseBool = false
+    }
+    if ($('#homeType').is(':checked')) {
+      type = 'home'
+    } else if ($('#buildingType').is(':checked')) {
+      type = 'building'
+    } else if ($('#mallType').is(':checked')) {
+      type = 'mall'
+    } else {
+      type = 'no_type'
+    }
+
+    checkImgData()
+
+    var toSend = {
+      'image': imageData,
+      'position': {
+        'latitude': document.getElementById('latitude').value,
+        'longitude': document.getElementById('longitude').value
+      },
+      'license': licenseBool,
+      'type': type,
+      'comment': document.getElementById('comment').value
+    }
+
+    $.ajax({
+      type: 'POST',
+      url: urlForm,
+      data: toSend
+    }).done(function(msg) {
+      console.log(msg)
+    })
+  }
