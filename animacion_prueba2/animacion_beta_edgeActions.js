b8c9f7508de844c9786fe9a3e42744952ee785c7
Ruben Dario Avila Home <rubenavilah@homecs.co>
2013-10-26 22:41:13 -0500

    Animacion Prueba 3

---
+++ animacion_prueba2/animacion_beta_edgeActions.js
@@ -0,0 +1,10 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Rectangle}","click",function(sym,e){sym.play("bomba");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-8621381");
